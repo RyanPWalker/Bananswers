02327a1b31b2ee0602be15c994dca4361ed26e9c
Craig Walker <cwalker@overstock.com>
2017-09-20 17:27:04 -0600

    cleaning up code, splitting into two components, waiting for image to load to show text

---
+++ bananswers/src/Bananswer.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Bananswers = [
+    "",
+    "Yes",
+    "No",
+    "It is certain",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook is good",
+    "Reply hazy try again",
+    "Ask again later",
+    "It's better I don't tell you",
+    "Think of a better question",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My sources say no",
+    "The answer is no",
+    "Nay",
+    "Of course",
+    "Very doubtful",
+    "You need to think about this more",
+    "Nope",
+    "Go for it",
+    "If you're still not sure, then just don't",
+    "I love you"
+];
+
+export default class Bananswer extends PureComponent {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+      const { bananswerIndex, done } = this.props;
+      const bananswer = Bananswers[bananswerIndex];
+      return (
+        <View style={styles.container}>
+          <Text style={styles.imageLabel}>{done && bananswer}</Text>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    imageLabel: {
+        textAlign: 'center',
+        backgroundColor: 'white',
+        color: 'black',
+    },
+  });
--- bananswers/src/Home.js
+++ bananswers/src/Home.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
   Image,
-  TouchableHighlight,
   TouchableWithoutFeedback
 } from 'react-native';
+import Bananswer from './Bananswer';
 
 const Images = [
     {
@@ -21,97 +20,69 @@ const Images = [
     }
 ];
 
-const Bananswers = [
-    "",
-    "Yes",
-    "No",
-    "It is certain",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook is good",
-    "Reply hazy try again",
-    "Ask again later",
-    "It's better I don't tell you",
-    "Think of a better question",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My sources say no",
-    "The answer is no",
-    "Nay",
-    "Of course",
-    "Very doubtful",
-    "You need to think about this more",
-    "Nope",
-    "Go for it",
-    "If you're still not sure, then just don't",
-    "I love you"
-];
-
-export default class Home extends Component {
+export default class Home extends PureComponent {
 	state = {
     index: 0,
     bananswerIndex: 0,
-    imageWidth: null
+    imageWidth: null,
+    done: false,
   }
 
-  nextImage(event) {
-    const { index, bananswerIndex, imageWidth } = this.state;
+  nextImage = (event) => {
+    const { index } = this.state;
     let newIndex = 0;
 
-    if (index == 0) { newIndex = 1; }
-    else { newIndex = 0; }
-    console.log('index: ' + index + ' newIndex: ' + newIndex);
+    if (index === 0) { newIndex = 1; }
 
-    this.setState({
-        index: newIndex,
-        bananswerIndex: 0
-    });
-    console.log('function called');
-
-    this.nextBananswer(newIndex, bananswerIndex);
+    this.nextBananswer(newIndex);
   }
 
-  nextBananswer(index, bananswerIndex) {
+  nextBananswer(index) {
     const min = 1;
     const max = 23;
     let newIndex = 0;
-    console.log('nextBananswer');
-    console.log('index: ' + index);
 
-    if (index == 0) { }
-    else { newIndex = Math.floor(Math.random() * (max - min)) + min; }
+    if (index !== 0) {
+      newIndex = Math.floor(Math.random() * (max - min)) + min;
+    }
 
     this.setState({
-      bananswerIndex: newIndex
+      bananswerIndex: newIndex,
+      index,
+      done: false,
     });
-
-    console.log('bananswerIndex: ' + bananswerIndex);
 
   }
 
-  onImageLayout(event) {
+  onImageLayout = (event) => {
     this.setState({
       imageWidth: event.nativeEvent.layout.width
     });
   }
 
+  handleLoad = () => {
+    this.setState({ done: true });
+  }
+
   render() {
-    const image = Images[this.state.index];
-    const bananswer = Bananswers[this.state.bananswerIndex];
-    console.log('rendering');
-    console.log('index: ' + this.state.index + ' bananswerIndex: ' + this.state.bananswerIndex);
+    const { index, bananswerIndex, done } = this.state;
+    const image = Images[index];
 
     return (
       <View style={styles.container}>
         <View style={styles.empty} />
-          <TouchableWithoutFeedback onPress={this.nextImage.bind(this)}
-          style={styles.image}>
-            <Image source={image.uri}
+          <TouchableWithoutFeedback
+            onPress={this.nextImage}
             style={styles.image}
-            onLayout={this.onImageLayout.bind(this)}>
-              <Text style={styles.imageLabel}>{bananswer}</Text>
-            </Image>
+          >
+            <Image
+              source={image.uri}
+              style={styles.image}
+              onLayout={this.onImageLayout}
+              onLoad={this.handleLoad}
+            />
           </TouchableWithoutFeedback>
+          <Bananswer bananswerIndex={bananswerIndex} done={done} />
         <View style={styles.empty} />
       </View>
     );
@@ -129,12 +100,6 @@ const styles = StyleSheet.create({
         width: 320,
         justifyContent: 'flex-end',
         alignItems: 'center'
-    },
-    imageLabel: {
-        textAlign: 'center',
-        backgroundColor: 'white',
-        color: 'black',
-        width: 320
     },
     empty: {
         flex: 1
